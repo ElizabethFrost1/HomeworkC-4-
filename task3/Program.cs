33dfb90e4400f7c2e7665c3ba62260b585ab7264
ElizabethFrost <liz.morozova2010@yandex.ru>
2023-09-15 13:02:42 +0300

    task3

---
+++ task3/Program.cs
@@ -0,0 +1,35 @@
+﻿// Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+//1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+//6, 1, 33 -> [6, 1, 33]
+
+int GetNumber(string message)
+{
+    Console.Write(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
+int size = GetNumber("Введите размер массива: ");
+int min = GetNumber("Введите минимальное значение в массиве: ");
+int max = GetNumber("Введите максимальное значение в массиве: ");
+
+
+int[] GetArray(int size, int min, int max)
+{
+    int[] array = new int[size];
+Random rand = new Random();
+for(int index =0; index < size; index++)
+{
+    array[index] = rand.Next(min, max);
+}
+return array;
+}
+void Print(int[]arr)
+{
+    int length = arr.Length;
+    for (int i = 0; i < length; i++)
+    {
+    Console.Write($"{arr[i]} ");
+
+    }
+}
+Print(GetArray(size, min, max));
+
